2d00f78909b853e39e19d482aa011f1d8669c775
yamuna1996d <yamunag19@gmail.com>
2020-01-17 12:59:58 +0530

    dummy programs

---
+++ check.py
@@ -0,0 +1,5 @@
+a=20
+if(a%2==0):
+    print("Even number")
+else:
+    print("Odd number")
---
+++ largest.py
@@ -0,0 +1,10 @@
+a=20
+b=30
+c=10
+if (a > b):
+    print(a)
+        
+elif (b > a): 
+    print(b)
+else: 
+    print(c)
